f273fbf8634ff728ef40fea3b6af67a886e8b428
Lionbrine <catsarecool2009@gmail.com>
2020-10-17 18:19:05 -0400

    Driver 1.5

--- driver.py
+++ driver.py
@@ -10,23 +10,17 @@ while("quit" not in userInput.lower()): #Read user Input
     Date = splitInput[1]
 
     stockTemp = "StockData " + Date 
-    reccomendationTemp = "Reccomendations " + Date 
 
     #Find Associated Data Files
     stockData = os.path.join(r'/DataBase', stockTemp)
-    reccomendationData = os.path.join(r'/DataBase', reccomendationTemp)
 
     #Validate Stock Data
     if(os.path.exists(stockData)):
         with open(stockData) as f:
             stockFile = f.read()
+            stockFile = stockFile.split("\n")
+            for line in stockFile:
+                if Stock in line:
+                    print(line)
     else:
         print("Could not find Data for that stock")
-    #Validate Reccomendation Data
-    if(os.path.exists(reccomendationData)):
-        with open(stockData) as f:
-            stockFile = f.read()
-    else:
-        print("Could not find Data for that stock")
-    
-    
